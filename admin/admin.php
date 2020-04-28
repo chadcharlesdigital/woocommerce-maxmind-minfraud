c25f3dab43199008097710eec6e466e5a97854fd
Chad Charles <chad@Chads-MacBook-Pro.local>
2020-04-28 16:59:47 -0600

    created basic woocommerce settings page

---
+++ admin/admin.php
@@ -0,0 +1,62 @@
+<?php
+use MaxMind\MinFraud;
+
+/**
+ * class that controls the admin interface
+ */
+class WMMMF_admin
+{
+    public function __construct()
+    {
+        add_filter('woocommerce_settings_tabs_array', array($this, 'add_settings_tab'), 50);
+        add_action( 'woocommerce_settings_tabs_woocommerce_maxmind_minfraud', array($this, 'settings_tab') );
+        add_action( 'woocommerce_update_options_woocommerce_maxmind_minfraud', array($this, 'update_settings') );
+    }
+
+    public function add_settings_tab($settings_tabs)
+    {
+        $settings_tabs['woocommerce_maxmind_minfraud'] = __('Maxmind MinFraud', 'woocommerce');
+        return $settings_tabs;
+    }
+
+    public function settings_tab()
+    {
+        woocommerce_admin_fields($this->get_settings());
+    }
+    public function update_settings()
+    {
+        woocommerce_update_options($this->get_settings());
+    }
+
+    public function get_settings()
+    {
+        $settings = array(
+          'section_title' => array(
+              'name'     => __('Maxmind Minfraud For Woocommerce', 'woocommerce'),
+              'type'     => 'title',
+              'desc'     => 'Reduce fraud in your store',
+              'id'       => 'wc_settings_tab_demo_section_title'
+          ),
+          'account_ID' => array(
+              'name' => __('Account ID', 'woocommerce'),
+              'type' => 'text',
+              'desc' => __('Maxmind Account/User ID found in your maxmind account', 'woocommerce'),
+              'id'   => 'wc_settings_tab_demo_title'
+          ),
+          'description' => array(
+              'name' => __('License Key', 'woocommerce'),
+              'type' => 'text',
+              'desc' => __('License key generated on the backend', 'woocommerce'),
+              'id'   => 'wc_settings_tab_demo_description'
+          ),
+          'section_end' => array(
+               'type' => 'sectionend',
+               'id' => 'wc_settings_tab_demo_section_end'
+          )
+        );
+        return $settings;
+        // return apply_filters('wc_settings_tab_demo_settings', $settings);
+    }
+}
+
+new WMMMF_admin;
--- woocommerce-maxmind-minfraud.php
+++ woocommerce-maxmind-minfraud.php
@@ -8,6 +8,7 @@
  * Requires PHP:      5.6
  * Author:            Chad Charles
  **/
+ use MaxMind\MinFraud;
 
 
 /**
@@ -18,7 +19,14 @@ class Woocommerce_Maxmind_Minfraud
 
   function __construct()
   {
-    
+    $this->load_dependencies();
+  }
+
+  public function load_dependencies()
+  {
+    //require composer autoload
+    require 'vendor/autoload.php';
+    require ( plugin_dir_path( __FILE__ ) . 'admin/admin.php');
   }
 }
 
