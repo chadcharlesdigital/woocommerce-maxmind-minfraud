e8b0a0f1c8054d307f5cbf82463834e1dff50635
Chad Charles <chad@Chads-MacBook-Pro.local>
2020-04-28 15:57:17 -0600

    inital commit

---
+++ woocommerce-maxmind-minfraud.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Plugin Name:       Woocommerce Maxmind Minfraud
+ * Plugin URI:
+ * Description:       Use Maxmind Minfraud score to do things with orders
+ * Version:           1.0
+ * Requires at least: 5.2
+ * Requires PHP:      5.6
+ * Author:            Chad Charles
+ **/
+
+
+/**
+ *
+ */
+class Woocommerce_Maxmind_Minfraud
+{
+
+  function __construct()
+  {
+    
+  }
+}
+
+new Woocommerce_Maxmind_Minfraud;
